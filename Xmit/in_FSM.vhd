f3ee3af62c370a639c13969fbbe5c0f98524bca2
Bridget Dou <bridget.dou@gmail.com>
2016-11-07 21:04:09 -0500

    clock-domain and gitignore

--- Xmit/in_FSM.vhd
+++ Xmit/in_FSM.vhd
@@ -3,57 +3,72 @@ use ieee.std_logic_1164.all;
 
 entity in_FSM is 
 port(
-	in_frame:				in std_logic_vector(7 downto 0);
 	in_priority:			in std_logic;
-	in_low_overflow:		in std_logic;
+	in_lo_overflow:		in std_logic;
 	in_hi_overflow:		in std_logic;
-	in_f_rdv:				in std_logic;
+	in_ctrl_write:			in std_logic;
 	out_m_discard_en:		out std_logic;
-	--out_m_discard_frame:	out std_logic_vector(7 downto 0);
-	out_frame:				out std_logic_vector(7 downto 0);
 	out_wren:				out std_logic;
 	out_priority: 			out std_logic;
+	clk_phy:					in std_logic;
 	clk_sys:					in std_logic;
 	reset:					in std_logic			
 );
 end in_FSM;
 
 architecture arch of in_FSM is
-	type numstate is (start_state, frame_state, end_state);
+--	type numstate is (start_state, frame_state, end_state);
+	signal discard_en_next: std_logic;
+	signal wren_next:			std_logic;
+	signal priority_next:	std_logic;
 	
 begin
 
 	process(clk_sys, reset)
 	begin
 		if(reset = '1') then
-			out_m_discard_en <= '0';
-		elsif (clk_sys'event and clk_sys='1') then
-			if ((in_low_overflow = '1' and in_priority='0') or (in_hi_overflow='1' and in_priority='1')) then
-				out_m_discard_en <= '1';
+			out_m_discard_en <= '0';		
+			out_wren <= '0';
+			out_priority <= '0';
+		elsif (clk_sys'event and clk_sys='1' and in_ctrl_write='1') then
+			priority_next<=in_priority;
+			if ((in_lo_overflow = '1' and in_priority='0') or (in_hi_overflow='1' and in_priority='1')) then
+				discard_en_next <= '1';
+				wren_next <= '0';
 			else
-				out_m_discard_en <= '0';
+				discard_en_next <= '0';
+				wren_next <= '1';
 			end if;
 		end if;
 	end process;
 	
-	process(clk_sys, reset)
+	process(clk_phy, discard_en_next, wren_next, priority_next)
 	begin
-		if(reset = '1') then
-			out_wren <= '0';
-		elsif (clk_sys'event and clk_sys='1') then
-			out_wren <= in_f_rdv;
+		if(clk_phy'event and clk_phy='1') then
+			out_priority<=priority_next;
+			out_m_discard_en<=discard_en_next;
+			out_wren<=wren_next;
 		end if;
 	end process;
 	
-	process(clk_sys, reset) begin
-		out_frame <= in_frame;
-		out_priority <= in_priority;
-	end process;
-	/*
-	process(clk_sys, reset) begin
-		if(reset = '1') then
-			
-		end if;
-	end process;
-	*/
+--	process(clk_sys, reset)
+--	begin
+--		if(reset = '1') then
+--			out_wren <= '0';
+--		elsif (clk_sys'event and clk_sys='1') then
+--			out_wren <= in_f_rdv;
+--		end if;
+--	end process;
+--	
+--	process(clk_sys, reset) begin
+--		out_frame <= in_frame;
+--		out_priority <= in_priority;
+--	end process;
+--	
+--	process(clk_sys, reset) begin
+--		if(reset = '1') then
+--			
+--		end if;
+--	end process;
+	
 end arch;
