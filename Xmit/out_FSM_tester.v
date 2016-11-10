66b7047445fc26e6f61b895d1b722dfa038e73b8
Kevin Liao <Kevin Liao>
2016-11-09 19:58:03 -0500

    new test file

--- Xmit/out_FSM_tester.v
+++ Xmit/out_FSM_tester.v
@@ -1,14 +1,19 @@
 module out_FSM_tester(
 input clk,
+input clk_phy,
+input clock_select,
 input rst_n,
 input [7:0] data_in,
 output [3:0] data_out,
+output [3:0] data_out_LEDs,
 output clk_out,
 output rst_out
 );
 
+assign clock = clock_select ? clk : clk_phy;
+
 out_FSM out_FSM(
-.clk_phy(clk),
+.clk_phy(clock),
 .reset(!rst_n),
 .data_in(data_in),
 .data_out(data_out),
@@ -18,5 +23,6 @@ out_FSM out_FSM(
 
 assign clk_out = clk;
 assign rst_out = !rst_n;
+assign data_out_LEDs = data_out;
 
 endmodule
