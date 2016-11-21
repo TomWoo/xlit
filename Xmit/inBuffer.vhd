050950e823e2b0fa8f76d8fd5cbe29a076dad352
Kevin Liao <Kevin Liao>
2016-11-21 17:59:02 -0500

    two registers left before testing

--- Xmit/inBuffer.vhd
+++ Xmit/inBuffer.vhd
@@ -18,10 +18,12 @@ end inBuffer;
 	
 architecture arch of inBuffer is
 	signal hi: std_logic := '1';
-	signal empty: std_logic;
-	signal full: std_logic;
+	signal emptyd, emptyc: std_logic;
+	signal fulld, fullc: std_logic;
 	signal incountdone: std_logic;
 	signal outcountdone: std_logic;
+	signal datam: std_logic_vector (7 downto 0);
+	signal ctrlm: std_logic_vector (23 downto 0);
 	
 
 	component inbuff 
@@ -35,18 +37,30 @@ architecture arch of inBuffer is
 		rdempty		: OUT STD_LOGIC ;
 		wrfull		: OUT STD_LOGIC 
 		);
-	end component;                                                                                                                                                        
+	end component;   
+	
+	component inbuffcon
+		port(	aclr		: IN STD_LOGIC  := '0';
+			data		: IN STD_LOGIC_VECTOR (23 DOWNTO 0);
+			rdclk		: IN STD_LOGIC ;
+			rdreq		: IN STD_LOGIC ;
+			wrclk		: IN STD_LOGIC ;
+			wrreq		: IN STD_LOGIC ;
+			q		: OUT STD_LOGIC_VECTOR (23 DOWNTO 0);
+			rdempty		: OUT STD_LOGIC ;
+			wrfull		: OUT STD_LOGIC 
+		);
+	END component;
 	
 begin 
 
-	PROCESS (sysclk, controli, wrenc, outcountdone) --incounter
+	PROCESS (sysclk, controli, wrenc) --incounter
 			VARIABLE   cnt         : INTEGER RANGE 0 TO 4096;
 			VARIABLE   direction    : INTEGER:=-1;
-	BEGIN
-			
+	BEGIN		
 		IF (sysclk'EVENT AND sysclk = '1') THEN
 			if (wrenc = '1') then
-				cnt := to_integer(unsigned(controli));
+				cnt := to_integer(unsigned(controli(11 downto 0)));
 			else
 				if (cnt>0) then
 					cnt := cnt + direction;
@@ -58,21 +72,59 @@ begin
 		else 
 			incountdone <= '0';
 		end if;
-			
-		
 	END PROCESS;
 
+	PROCESS (phyclk, controli, outcountdone) --outcounter
+			VARIABLE   cnt         : INTEGER RANGE 0 TO 4096;
+			VARIABLE   direction    : INTEGER:=-1;
+	BEGIN	
+		IF (phyclk'EVENT AND phyclk = '1') THEN
+			if (outcountdone = '1') then
+				cnt := to_integer(unsigned(ctrlm(11 downto 0)));
+			else
+				if (cnt>0) then
+					cnt := cnt + direction;
+				end if;
+			end if;
+		END IF;
+		if (cnt <= 1) then
+			outcountdone <= '1';
+		else 
+			outcountdone <= '0';
+		end if;
+	END PROCESS;
+	
+	process --ctrlout
+	begin
+	end process;
+	
+	process --dataout
+	begin
+	end process;
+	
 	inbuff_comp : inbuff
 		port map (
 			aclr => aclr,
 			wrclk => sysclk,
 			rdclk => phyclk,
 			q => datao,
-			data => datai,
+			data => datam,
 			wrreq => wrend,
 			rdreq => outcountdone,
-			rdempty => empty,
-			wrfull => full
+			rdempty => emptyd,
+			wrfull => fulld
 			);
-
+			
+	inbuffcon_comp: inbuffcon
+		port map(
+			aclr => aclr,
+			wrclk => sysclk,
+			rdclk => phyclk,
+			q => controlo,
+			data => ctrlm,
+			wrreq => wrenc,
+			rdreq => outcountdone,
+			rdempty => emptyc,
+			wrfull => fullc
+		);
 end arch;
